7736a2bf575facfa44dd2a599fec3393903e8a6d
Hajali Bayram <hajalibayram@outlook.com>
2017-10-15 10:57:02 +0400

    Commit test4

--- app/src/main/java/hajalibayram/hnotes_android/activity/DocumentActivity.java
+++ app/src/main/java/hajalibayram/hnotes_android/activity/DocumentActivity.java
@@ -2,8 +2,11 @@ package hajalibayram.hnotes_android.activity;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.webkit.WebView;
 
 import hajalibayram.hnotes_android.R;
@@ -38,6 +41,28 @@ public class DocumentActivity extends AppCompatActivity {
         mWebView = (WebView) findViewById(R.id.web_view);
         mWebView.loadData(mUrlStr, "text/html; charset=utf-8", "UTF-8");
 
+        findViewById(R.id.doc_share).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        share(mUrlStr);
+
+    }
+
+    public void share(String text) {
+        String mimeType = "text/plain";
+        String title = "Select one";
+
+        Intent shareIntent = ShareCompat.IntentBuilder.from(this)
+                .setChooserTitle(title)
+                .setType(mimeType)
+                .setText(text)
+                .getIntent();
+        if (shareIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(shareIntent);
+        }
     }
 
 
